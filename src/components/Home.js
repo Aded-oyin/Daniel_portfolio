9d6ffff198d59c48b2b8cabc4a4b8559ffcb9e18
Aded-oyin <danyodele50@gmail.com>
2023-11-08 19:57:47 +0100

    Updated

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,7 +6,7 @@ const Home = () => {
   return (
     <div name="home" className='w-full h-screen bg-gradient-to-b from-[#0a192f] via-[#0a192f] to-slate-700'>
 
-         <div className='max-w-[1000px] mx-auto px-20 lg:px-8 flex flex-col justify-center h-full'>
+         <div className='max-w-[1000px] mx-auto px-10 lg:px-8 flex flex-col justify-center h-full'>
             <p className='text-[cyan]'>Hi, my name is</p>
             <h1 className='text-4xl sm:text-7xl font-bold text-[#ccd6f6]'>Daniel Ayodele</h1>
             <h2 className='text-4xl sm:text-7xl font-bold text-[#8892b0]'>I'm a Frontend Developer</h2>
