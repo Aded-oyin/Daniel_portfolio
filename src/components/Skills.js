4c7a4f1990015fc42d8dc9baa404f7ca0168c3f5
Aded-oyin <danyodele50@gmail.com>
2024-05-18 00:41:07 +0100

    updated

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -4,7 +4,7 @@ import css from '../assets/css.png'
 import javascript from '../assets/javascript.png'
 import react from '../assets/react.png'
 import tailwind from '../assets/tailwind.png'
-import github from '../assets/github.png'
+import firebase from '../assets/firebase.png'
 
 const Skills = () => {
   return (
@@ -13,7 +13,7 @@ const Skills = () => {
         <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
             <div>
                 <p className='text-4xl font-bold inline border-b-4 border-purple-300'>Skills</p>
-                <p className='py-4'>// These are the technologies I've worked with.</p>
+                <p className='py-4'>// These are some technologies I've worked with.</p>
             </div>
 
             <div className='w-full grid grid-cols-2 sm:grid-cols-3 gap-4 text-center py-8'>
@@ -38,7 +38,7 @@ const Skills = () => {
                     <p className='my-4'>TAILWIND</p>
                 </div>
                 <div className='shadow-md shadow-gray-500 hover:scale-105 duration-500 p-2 rounded-lg'>
-                    <img className='w-20 mx-auto' src={github} alt="HTML icon"/>
+                    <img className='w-20 mx-auto' src={firebase} alt="HTML icon"/>
                     <p className='my-4'>GITHUB</p>
                 </div>
             </div>
